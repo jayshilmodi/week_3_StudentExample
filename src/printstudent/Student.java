a49655d277963471db77db08afafaa4555e6a67e
Jaysheel Modi <42139355+jayshilmodi@users.noreply.github.com>
2022-05-25 15:03:07 -0400

    Update Student.java

--- src/printstudent/Student.java
+++ src/printstudent/Student.java
@@ -1,4 +1,4 @@
-//25 may 2022
+//25 may
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
