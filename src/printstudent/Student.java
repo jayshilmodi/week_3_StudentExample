f4bdb896e384fb493edbc2039cb6b6057ffe9131
jayshilmodi <jayshilmodi999@gmail.com>
2022-05-25 14:49:58 -0400

    version 0.2

--- src/printstudent/Student.java
+++ src/printstudent/Student.java
@@ -1,3 +1,4 @@
+//25 may 2022
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
